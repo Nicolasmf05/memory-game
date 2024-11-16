3deffd4107431176ce5787845aec225dc197c862
Nicolas <nicolasmejiafernandez@gmail.com>
2024-11-17 00:30:26 +0100

    Solucionado problemas de imagenes

--- memory-game/src/componentes/Card.js
+++ memory-game/src/componentes/Card.js
@@ -2,7 +2,7 @@ function Card({item, id, handleClick}) {
     const itemClass = item.stat ? "active" : "";
     return (
         <div className={"card" + itemClass} onClick={() => handleClick(id)}>
-            <img src={item.img} alt="" />
+            <img src={item.img} alt="" className="card-img" />
         </div>
     )
 }
--- memory-game/src/componentes/Cards.js
+++ memory-game/src/componentes/Cards.js
@@ -3,21 +3,22 @@ import Card from "./Card";
 function Cards(){
 
     const[items, setItems]= useState([
-        {id:1,nombre:"Homer",imagen:"/img/654e78adef00847c156ab367b870bbe3.jpg", stat: ""},
-        {id:1,nombre:"Homer",imagen:"/img/78/654e78adef00847c156ab367b870bbe3.jpg", stat: ""},
-        {id:2,nombre:"Bart",imagen:"/img/Homer-Simpson-PNG-Download-Image.png", stat: ""},
-        {id:2,nombre:"Bart",imagen:"/img/Homer-Simpson-PNG-Download-Image.png", stat: ""},
-        {id:3,nombre:"Marge",imagen:"/img/classic-cartoon-marge-simpson-o9csammi6z9rdt8u.jpg", stat: ""},
-        {id:3,nombre:"Marge",imagen:"/img/classic-cartoon-marge-simpson-o9csammi6z9rdt8u.jpg", stat: ""},
-        {id:4,nombre:"Lisa",imagen:"/img/b56f8a8011478a322b430e540db3e727.jpg", stat: ""},
-        {id:4,nombre:"Lisa",imagen:"/img/b56f8a8011478a322b430e540db3e727.jpg", stat: ""}
+        //id de la img de homer
+        {id:1,nombre:"Homer",img:"/img/homer.png", stat: ""},
+        {id:1,nombre:"Homer",img:"/img/homer.png", stat: ""},
+        {id:2,nombre:"Bart",img:"/img/bart.jpg", stat: ""},
+        {id:2,nombre:"Bart",img:"/img/bart.jpg", stat: ""},
+        {id:3,nombre:"Marge",img:"/img/marge.jpg", stat: ""},
+        {id:3,nombre:"Marge",img:"/img/marge.jpg", stat: ""},
+        {id:4,nombre:"Lisa",img:"/img/lisa.jpg", stat: ""},
+        {id:4,nombre:"Lisa",img:"/img/lisa.jpg", stat: ""}
         
     ].sort(() => Math.random() - 0.5)) ;
 
     const [prev, setPrev] = useState(-1)
 
     function check(current){
-        if(items[current].id === items[prev].id){
+        if(items[current].id == items[prev].id){
             items[current].stat = "correct"
             items[prev].stat = "correct"
             setItems([...items])
@@ -30,11 +31,10 @@ function Cards(){
                 items[current].stat = ""
                 items[prev].stat = ""
                 setItems([...items])
-
                 setPrev(-1)
-        },1000)
+            }, 1000)
+        }
     }
-}
 
     function handleClick(id){
         if(prev === -1){
@@ -48,15 +48,14 @@ function Cards(){
 
     return (
         <div className="container">
-            {  items.map((item, index)=> (
-                <Card key={index} item={item} id={index} handleClick={handleClick}/>
-            ))}
-
+            { items.map((item, index) => (
+                <Card key={index} item={item} id={index} handleClick={handleClick} />
+            )) }
         </div>
     )
 }
 
-export default Cards;
+export default Cards
 
 
 
